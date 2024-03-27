559f4bf2b4ae2f067246428337c59ec372fe4966
mtembe <mathieu.geipel@gmail.com>
2024-03-27 11:24:22 +0100

    changed redis acces & workflow

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
 	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
+		Addr:     "my-release-redis-master.default.svc.cluster.local:6379",
+		Password: "13371337",
 		DB:       0,
 	})
 
